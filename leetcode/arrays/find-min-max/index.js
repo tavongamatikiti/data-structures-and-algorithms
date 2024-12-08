87c48e25e558cdd247a3adbf068c6d802dbcee5f
tavongamatikiti <tavongamatikiti@fgmail.com>
2024-12-08 11:35:40 +0200

    Fixed to findMinMax

--- leetcode/arrays/find-min-max/index.js
+++ leetcode/arrays/find-min-max/index.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums2
  * @return {{min: number, max: number}}
  */
-const findMedianSortedArrays = function (nums1, nums2) {
+const findMinMax = function (nums1, nums2) {
   const mergedArr = [...nums1, ...nums2].sort((a, b) => a - b);
   if (mergedArr.length === 0) return { min: null, max: null };
 
@@ -16,8 +16,8 @@ const findMedianSortedArrays = function (nums1, nums2) {
   };
 };
 
-console.log(findMedianSortedArrays([1, 3], [2]));
-console.log(findMedianSortedArrays([], [3, 4]));
-console.log(findMedianSortedArrays([7, 8], [1, 2, 9]));
-console.log(findMedianSortedArrays([], []));
-console.log(findMedianSortedArrays([5], [5]));
+console.log(findMinMax([1, 3], [2]));
+console.log(findMinMax([], [3, 4]));
+console.log(findMinMax([7, 8], [1, 2, 9]));
+console.log(findMinMax([], []));
+console.log(findMinMax([5], [5]));
